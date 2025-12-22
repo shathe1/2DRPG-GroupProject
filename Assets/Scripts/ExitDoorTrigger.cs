24ad9e821620f8102f615e5c6f7b8b13c7319bfe
Chadha Khalouaoui <SWE2309433@xmu.edu.my>
2025-12-22 09:35:35 +0800

    fixed

--- Assets/Scripts/ExitDoorTrigger.cs
+++ Assets/Scripts/ExitDoorTrigger.cs
@@ -12,10 +12,12 @@ public class ExitDoor : MonoBehaviour
 
         triggered = true;
 
-        if (HeartManager.Instance != null &&
-            HeartManager.Instance.currentLives > 0)
-        {
-            SceneManager.LoadScene("WinScreen");
-        }
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+
+        PlayerPrefs.SetInt("CurrentLevelIndex", currentIndex);
+        PlayerPrefs.SetInt("NextLevelIndex", currentIndex + 1);
+        PlayerPrefs.Save();
+
+        SceneManager.LoadScene("WinScreen");
     }
 }
--- Assets/Scripts/Level3Timer.cs
+++ Assets/Scripts/Level3Timer.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TimerController : MonoBehaviour
 {
     public float timeRemaining = 30f;
+    public TextMeshProUGUI timerText;
 
     void Update()
     {
         if (timeRemaining > 0)
         {
             timeRemaining -= Time.deltaTime;
+            UpdateTimerUI();
         }
         else
         {
             timeRemaining = 0;
+            UpdateTimerUI();
 
             int currentIndex = SceneManager.GetActiveScene().buildIndex;
             PlayerPrefs.SetInt("NextLevelIndex", currentIndex + 1);
@@ -21,5 +25,10 @@ public class TimerController : MonoBehaviour
 
             SceneManager.LoadScene("WinScreen");
         }
+    }
+
+    void UpdateTimerUI()
+    {
+        timerText.text = Mathf.Ceil(timeRemaining).ToString();
     }
 }
